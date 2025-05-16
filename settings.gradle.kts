9f24350a01cc5d936535bfbb5f957d1f491af629
longjianghu <215241062@qq.com>
2025-05-16 21:21:23 +0800

    feat:添加插件文件

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
+rootProject.name = "kebabCase"
